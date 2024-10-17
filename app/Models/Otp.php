61f756d493b79f4d8f457df76954fe0e26d90e1a
rey <arynldimhmmd@gmail.com>
2024-10-17 15:13:49 +0700

    logo

---
+++ app/Models/Otp.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Otp extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'phone_number',
+        'otp_code',
+        'expires_at',
+    ];
+}
--- resources/views/Parents/index.blade.php
+++ resources/views/Parents/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $setting?->app_name ?? config('app.name') }} - Parents Admin</title>  
-    <link rel="icon" href="{{ $setting->app_logo ? asset('storage/' . $setting->app_logo) : asset('default_favicon.ico') }}" type="image/png">
+    <link rel="icon" href="{{ $setting?->app_logo ? asset('storage/' . $setting?->app_logo) : asset('default_favicon.ico') }}" type="image/png">
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap" rel="stylesheet">
     <!-- Bootstrap CSS -->
